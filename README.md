5214599903c6a241eaa80264e3d67445337b6ec5
msyd27 <msyd27@hotmail.com>
2024-04-13 19:43:42 -0400

    Added project files to repo

---
+++ README.md
@@ -0,0 +1,14 @@
+# CPS 847 Assignment 3
+
+Vue Project Source Code: [main branch] [part1-vue > src](https://github.com/msyd27/CPS847-A3/tree/main/part1-vue/src)
+
+Django Project Source Code: https://github.com/msyd27/CPS847-A3/tree/main/django
+
+Original SQL Script: https://github.com/msyd27/CPS847-A3/blob/main/django/SQLoriginalScript
+
+Migration SQL Script: https://github.com/msyd27/CPS847-A3/blob/main/django/SQLmigrateScript
+
+Vue App Deployment URL: [http://192.168.57.172:5173](http://192.168.57.172:5173)
+
+Django App Deployment URL: http://192.168.61.33:8000/admin
+(username: admin    password:12345)
---
+++ part1-vue/src/components/Home.vue
@@ -0,0 +1,43 @@
+<script>
+import { format } from 'date-fns';
+
+const API_URL = `https://api.openweathermap.org/data/2.5/weather?lat=43.1940&lon=-80.3845&appid=8ad493940057ce1f08e48316a3194a8c&units=metric`
+
+export default {
+  data () {
+    return {
+      format,
+      weather: null
+    }
+  },
+
+  created() {
+    this.fetchData()
+  },
+
+  methods: {
+    async fetchData() {
+      const weatherData = await (await fetch(API_URL)).json()
+      this.weather = Math.round(weatherData.main.temp)
+    },
+    getCurrentDateTime() {
+      return format(new Date(), 'yyyy-MM-dd HH:mm:ss');
+    }
+  }
+}
+
+</script>
+
+<template>
+
+  <WelcomeItem>
+    
+    <h2>Date/Time</h2>
+    <p>The current date and time is: {{getCurrentDateTime()}}</p>
+    <br>
+    <h2>Temperature</h2>
+    <p> The current temperature in Paris, Ontario is: {{ weather }}°C</p>
+  </WelcomeItem>
+
+  
+</template>
